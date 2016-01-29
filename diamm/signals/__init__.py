a7f5cbce2673e8af058f10590cda0f0dcde1162a
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-01-29 16:02:31 +0000

    Add composition indexing signals

--- diamm/signals/__init__.py
+++ diamm/signals/__init__.py
@@ -1,3 +1,4 @@
 import diamm.signals.source_signals
 import diamm.signals.archive_signals
 import diamm.signals.person_signals
+import diamm.signals.composition_signals
---
+++ diamm/signals/composition_signals.py
@@ -0,0 +1,15 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
+from diamm.models.data.composition import Composition
+from diamm.serializers.search.composition import CompositionSearchSerializer
+from diamm.helpers.solr_helpers import solr_delete, solr_index
+
+
+@receiver(post_save, sender=Composition)
+def index_archive(sender, instance, created, **kwargs):
+    solr_index(CompositionSearchSerializer, instance)
+
+
+@receiver(post_delete, sender=Composition)
+def delete_archive(sender, instance, **kwargs):
+    solr_delete(CompositionSearchSerializer, instance)
