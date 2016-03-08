a864960adc84025558f5c0ad0621e304bba311cb
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-03-08 17:12:17 +0000

    Provenance indexing

---
+++ diamm/serializers/search/provenance.py
@@ -0,0 +1,69 @@
+import serpy
+from diamm.models.data.person import Person
+from diamm.models.data.organization import Organization
+
+
+class SourceProvenanceSerializer(serpy.Serializer):
+    type = serpy.MethodField()
+    pk = serpy.IntField()
+
+    source_i = serpy.IntField(
+        attr="source.pk"
+    )
+
+    city_s = serpy.MethodField()
+    country_s = serpy.MethodField()
+    region_s = serpy.MethodField()
+    protectorate_s = serpy.MethodField()
+
+    uncertain_b = serpy.BoolField(
+        attr="uncertain",
+        required=False
+    )
+    earliest_year_i = serpy.IntField(
+        attr="earliest_year",
+        required=False
+    )
+    latest_year_i = serpy.IntField(
+        attr="latest_year",
+        required=False
+    )
+    entity_s = serpy.MethodField()
+
+    def get_city_s(self, obj):
+        if obj.city:
+            return obj.city.name
+        return None
+
+    def get_country_s(self, obj):
+        if obj.country:
+            return obj.country.name
+        return None
+
+    def get_region_s(self, obj):
+        if obj.region:
+            return obj.region.name
+        return None
+
+    def get_protectorate_s(self, obj):
+        if obj.protectorate:
+            return obj.protectorate.name
+        return None
+
+    def get_entity_s(self, obj):
+        objtype = ""
+        name = ""
+
+        if isinstance(obj.entity, Organization):
+            objtype = "organization"
+            name = obj.entity.name
+        elif isinstance(obj.entity, Person):
+            objtype = "person"
+            name = obj.entity.full_name
+        else:
+            return None
+
+        return "{0}|{1}|{2}".format(name, obj.pk, objtype)
+
+    def get_type(self, obj):
+        return obj.__class__.__name__.lower()
