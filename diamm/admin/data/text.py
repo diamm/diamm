4cddeef0b18ed29af8d8058635e099eee5d5e88a
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-03-09 17:00:59 +0000

    Update admin configs

--- diamm/admin/data/text.py
+++ diamm/admin/data/text.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.db import models
 from diamm.models.data.text import Text
+from reversion.admin import VersionAdmin
+from pagedown.widgets import AdminPagedownWidget
 
 
 @admin.register(Text)
-class TextAdmin(admin.ModelAdmin):
+class TextAdmin(VersionAdmin):
     list_display = ('incipit',)
     search_fields = ('incipit', 'text')
+
+    formfield_overrides = {
+        models.TextField: {'widget': AdminPagedownWidget}
+    }
