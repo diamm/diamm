2d2ba65500d37b42ce69ec899d2a7f1c2eeb1eea
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-03-10 13:38:20 +0000

    Updated admin view of source relationships

--- diamm/admin/data/source_relationship.py
+++ diamm/admin/data/source_relationship.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from diamm.models.data.source_relationship import SourceRelationship
+from diamm.models.data.person import Person
+from diamm.models.data.organization import Organization
 from django_extensions.admin import ForeignKeyAutocompleteAdmin
 
 
 @admin.register(SourceRelationship)
 class SourceRelationshipAdmin(ForeignKeyAutocompleteAdmin):
-    pass
-    # list_display = ('get_source', 'get_person', 'relationship_type')
+    list_display = ('get_source', 'get_related_entity', 'relationship_type')
+
     # search_fields = ('source__name',
     #                  'source__identifiers__identifier',
     #                  'source__archive__name',
@@ -17,10 +19,16 @@ class SourceRelationshipAdmin(ForeignKeyAutocompleteAdmin):
     #     'person': ('last_name',)
     # }
 
-    # def get_source(self, obj):
-    #     return "{0}".format(obj.source.display_name)
-    # get_source.short_description = "source"
-    #
-    # def get_person(self, obj):
-    #     return "{0}".format(obj.person.full_name)
-    # get_person.short_description = "person"
+    def get_source(self, obj):
+        return "{0}".format(obj.source.display_name)
+    get_source.short_description = "source"
+
+    def get_related_entity(self, obj):
+        if isinstance(obj.related_entity, Organization):
+            return "{0} (organization)".format(obj.related_entity.name)
+        elif isinstance(obj.related_entity, Person):
+            return "{0} (person)".format(obj.related_entity.full_name)
+        else:
+            return None
+
+    get_related_entity.short_description = "Related Entity"
