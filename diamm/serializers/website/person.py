846c89a4395f7b95031c0c1c11523aa7726059f0
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-03-15 09:53:47 +0000

    WIP: Migrate person serializer to serpy

--- diamm/serializers/website/person.py
+++ diamm/serializers/website/person.py
@@ -1,109 +1,178 @@
-from rest_framework import serializers
-from diamm.models.data.person import Person
-from diamm.models.data.person_note import PersonNote
-from diamm.models.data.composition_composer import CompositionComposer
-from diamm.models.data.source import Source
-from diamm.models.data.source_relationship import SourceRelationship
-from diamm.models.data.source_copyist import SourceCopyist
-from diamm.models.data.source_relationship_type import SourceRelationshipType
-from diamm.models.data.archive import Archive
+import serpy
+from rest_framework.reverse import reverse
+from diamm.serializers.serializers import ContextDictSerializer, ContextSerializer
 
 
-class PersonSourceCopiedSerializer(serializers.ModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name='source-detail',
-                                               source='source.id',
-                                               read_only=True)
+class PersonCompositionSerializer(ContextSerializer):
+    url = serpy.MethodField()
+    name = serpy.StrField(
+        attr='composition.name'
+    )
+    sources = serpy.MethodField()
+    uncertain = serpy.BoolField()
 
-    display_name = serializers.ReadOnlyField(source="source.display_name")
-    type = serializers.ReadOnlyField()
+    def get_url(self, obj):
+        return reverse('composition-detail',
+                       kwargs={"pk": obj.composition.pk},
+                       request=self.context['request'])
 
-    class Meta:
-        model = SourceCopyist
-        fields = ('url', 'display_name', 'type')
+    def get_sources(self, obj):
+        sources = []
+        if obj.composition.sources.count() == 0:
+            return None
+        for item in obj.composition.sources.all():
+            s = {
+                'url': reverse('source-detail',
+                               kwargs={"pk": item.source.pk},
+                               request=self.context['request']),
+                'display_name': item.source.display_name
+            }
+            sources.append(s)
+        return sources
 
-
-class PersonSourceRelationshipTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SourceRelationshipType
-        fields = ('name',)
-
-
-class PersonSourceRelationshipSerializer(serializers.ModelSerializer):
-    url = serializers.HyperlinkedRelatedField(view_name="source-detail",
-                                              source="source.id",
-                                              read_only=True)
-    display_name = serializers.ReadOnlyField(source="source.display_name")
-    archive = serializers.ReadOnlyField(source="source.archive.name")
-    relationship_type = PersonSourceRelationshipTypeSerializer()
-
-    class Meta:
-        model = SourceRelationship
-        fields = ('url', 'archive', 'display_name', 'relationship_type')
-
-
-class PersonSourceArchiveSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Archive
-        fields = ('url', 'name', 'city')
+class PersonListSerializer(ContextSerializer):
+    pass
 
 
-class PersonSourceSerializer(serializers.ModelSerializer):
-    url = serializers.HyperlinkedRelatedField(source="source.id",
-                                              view_name="source-detail",
-                                              read_only=True)
-    display_name = serializers.ReadOnlyField(source="source.display_name")
-    archive = PersonSourceArchiveSerializer(source="source.archive")
+class PersonDetailSerializer(ContextSerializer):
+    url = serpy.MethodField()
+    compositions = serpy.MethodField()
+    full_name = serpy.StrField()
+    earliest_year = serpy.IntField()
+    earliest_year_approximate = serpy.BoolField()
+    latest_year = serpy.IntField()
+    latest_year_approximate = serpy.BoolField()
+    appears_in = serpy.MethodField()
 
-    class Meta:
-        model = Source
-        fields = ('url', 'display_name', 'archive')
+    def get_url(self, obj):
+        return reverse('person-detail',
+                       kwargs={"pk": obj.pk},
+                       request=self.context['request'])
 
+    def get_compositions(self, obj):
+        return [PersonCompositionSerializer(o, context={'request': self.context['request']}).data for o in obj.compositions.all()]
 
-class PersonCompositionSerializer(serializers.ModelSerializer):
-    url = serializers.HyperlinkedRelatedField(view_name="composition-detail",
-                                              source="composition.id",
-                                              read_only=True)
-    name = serializers.ReadOnlyField(source="composition.name")
-    sources = PersonSourceSerializer(source="composition.sources", many=True)
-    uncertain = serializers.ReadOnlyField()
-
-    class Meta:
-        model = CompositionComposer
-        fields = ('url', 'name', 'sources', 'uncertain')
-
-
-class PersonNoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PersonNote
-        fields = ('note',)
-
-
-class PersonListSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Person
-        fields = ('url', 'full_name')
+    def get_appears_in(self, obj):
+        if obj.item_set.count() == 0:
+            return None
+        sources = []
+        for item in obj.item_set.all():
+            s = {
+                'url': reverse('source-detail',
+                               kwargs={"pk": item.source.pk},
+                               request=self.context['request']),
+                'display_name': item.source.display_name
+            }
+            sources.append(s)
+        return sources
 
 
-class PersonDetailSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Person
-        fields = ('url',
-                  'full_name',
-                  'compositions',
-                  'sources_related',
-                  'sources_copied',
-                  'biography',
-                  'dates')
+# from rest_framework import serializers
+# from diamm.models.data.person import Person
+# from diamm.models.data.person_note import PersonNote
+# from diamm.models.data.composition_composer import CompositionComposer
+# from diamm.models.data.source import Source
+# from diamm.models.data.source_relationship import SourceRelationship
+# from diamm.models.data.source_copyist import SourceCopyist
+# from diamm.models.data.source_relationship_type import SourceRelationshipType
+# from diamm.models.data.archive import Archive
 
-    compositions = PersonCompositionSerializer(many=True)
-    full_name = serializers.ReadOnlyField()
-    sources_related = PersonSourceRelationshipSerializer(many=True)
-    sources_copied = PersonSourceCopiedSerializer(many=True)
-    biography = serializers.SerializerMethodField()
-    dates = serializers.SerializerMethodField()
 
-    def get_biography(self, obj):
-        return PersonNoteSerializer(obj.notes.filter(type=PersonNote.BIOGRAPHY), many=True).data
-
-    def get_dates(self, obj):
-        return PersonNoteSerializer(obj.notes.filter(type=PersonNote.DATE_NOTE), many=True).data
+# class PersonSourceCopiedSerializer(serializers.ModelSerializer):
+#     url = serializers.HyperlinkedIdentityField(view_name='source-detail',
+#                                                source='source.id',
+#                                                read_only=True)
+#
+#     display_name = serializers.ReadOnlyField(source="source.display_name")
+#     type = serializers.ReadOnlyField()
+#
+#     class Meta:
+#         model = SourceCopyist
+#         fields = ('url', 'display_name', 'type')
+#
+#
+# class PersonSourceRelationshipTypeSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = SourceRelationshipType
+#         fields = ('name',)
+#
+#
+# class PersonSourceRelationshipSerializer(serializers.ModelSerializer):
+#     url = serializers.HyperlinkedRelatedField(view_name="source-detail",
+#                                               source="source.id",
+#                                               read_only=True)
+#     display_name = serializers.ReadOnlyField(source="source.display_name")
+#     archive = serializers.ReadOnlyField(source="source.archive.name")
+#     relationship_type = PersonSourceRelationshipTypeSerializer()
+#
+#     class Meta:
+#         model = SourceRelationship
+#         fields = ('url', 'archive', 'display_name', 'relationship_type')
+#
+#
+# class PersonSourceArchiveSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Archive
+#         fields = ('url', 'name', 'city')
+#
+#
+# class PersonSourceSerializer(serializers.ModelSerializer):
+#     url = serializers.HyperlinkedRelatedField(source="source.id",
+#                                               view_name="source-detail",
+#                                               read_only=True)
+#     display_name = serializers.ReadOnlyField(source="source.display_name")
+#     archive = PersonSourceArchiveSerializer(source="source.archive")
+#
+#     class Meta:
+#         model = Source
+#         fields = ('url', 'display_name', 'archive')
+#
+#
+# class PersonCompositionSerializer(serializers.ModelSerializer):
+#     url = serializers.HyperlinkedRelatedField(view_name="composition-detail",
+#                                               source="composition.id",
+#                                               read_only=True)
+#     name = serializers.ReadOnlyField(source="composition.name")
+#     sources = PersonSourceSerializer(source="composition.sources", many=True)
+#     uncertain = serializers.ReadOnlyField()
+#
+#     class Meta:
+#         model = CompositionComposer
+#         fields = ('url', 'name', 'sources', 'uncertain')
+#
+#
+# class PersonNoteSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = PersonNote
+#         fields = ('note',)
+#
+#
+# class PersonListSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Person
+#         fields = ('url', 'full_name')
+#
+#
+# class PersonDetailSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Person
+#         fields = ('url',
+#                   'full_name',
+#                   'compositions',
+#                   'sources_related',
+#                   'sources_copied',
+#                   'biography',
+#                   'dates')
+#
+#     compositions = PersonCompositionSerializer(many=True)
+#     full_name = serializers.ReadOnlyField()
+#     sources_related = PersonSourceRelationshipSerializer(many=True)
+#     sources_copied = PersonSourceCopiedSerializer(many=True)
+#     biography = serializers.SerializerMethodField()
+#     dates = serializers.SerializerMethodField()
+#
+#     def get_biography(self, obj):
+#         return PersonNoteSerializer(obj.notes.filter(type=PersonNote.BIOGRAPHY), many=True).data
+#
+#     def get_dates(self, obj):
+#         return PersonNoteSerializer(obj.notes.filter(type=PersonNote.DATE_NOTE), many=True).data
