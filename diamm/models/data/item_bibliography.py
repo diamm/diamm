e7b62c51d0ec19ae4608a4909465217b42ee5787
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-03-15 09:52:30 +0000

    New item bibliographries

---
+++ diamm/models/data/item_bibliography.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class ItemBibliography(models.Model):
+    class Meta:
+        app_label = "diamm_data"
+
+    item = models.ForeignKey("diamm_data.Item")
+    bibliography = models.ForeignKey("diamm_data.Bibliography")
+    pages = models.CharField(max_length=256, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
