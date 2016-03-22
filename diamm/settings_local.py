97f9ea1208e6abe0f704421156d77b4a7bbc3ef4
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-03-22 17:08:20 +0000

    Fix the MEDIA ROOT param

--- diamm/settings_local.py
+++ diamm/settings_local.py
@@ -2,7 +2,7 @@ import os
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(
-        os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "media"
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "diamm", "media"
 )
 STATIC_ROOT = "/srv/www/alpha.diamm.ac.uk/static"
 LEGACY_IMAGE_DIR = "/Users/ahankins/Documents/code/git/diamm/testing/legacy"
