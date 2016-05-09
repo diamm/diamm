bf2651a90827d68225e81f35c21d6c477215441f
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-05-09 13:55:39 -0400

    New: Add voice serialization in IIIF

--- diamm/serializers/iiif/annotation_list.py
+++ diamm/serializers/iiif/annotation_list.py
@@ -1,7 +1,45 @@
 import serpy
+import pysolr
+from django.conf import settings
 from rest_framework.reverse import reverse
 from diamm.serializers.serializers import ContextDictSerializer
 from diamm.serializers.fields import StaticField
+
+
+class AnnotationVoiceSerializer(ContextDictSerializer):
+    type = serpy.StrField(
+        attr='voice_type_s'
+    )
+    text = serpy.MethodField()
+    languages = serpy.MethodField()
+    clef = serpy.MethodField()
+    mensuration = serpy.MethodField()
+    mensuration_text = serpy.MethodField()
+
+    def get_text(self, obj):
+        if 'voice_text_s' in obj:
+            return obj['voice_text_s']
+        return None
+
+    def get_clef(self, obj):
+        if 'clef_s' in obj:
+            return obj['clef_s']
+        return None
+
+    def get_mensuration(self, obj):
+        if 'mensuration_s' in obj:
+            return obj['mensuration_s']
+        return None
+
+    def get_mensuration_text(self, obj):
+        if 'mensuration_text_s' in obj:
+            return obj['mensuation_text_s']
+        return None
+
+    def get_languages(self, obj):
+        if 'languages_ss' in obj:
+            return obj['languages_ss']
+        return []
 
 
 class AnnotationSerializer(ContextDictSerializer):
@@ -16,6 +54,8 @@ class AnnotationSerializer(ContextDictSerializer):
 
     def get_resource(self, obj):
         body = {}
+
+        body['voices'] = self._get_voice_data(obj)
 
         if 'folio_start_s' in obj:
             body['folio_start'] = obj['folio_start_s']
@@ -65,6 +105,23 @@ class AnnotationSerializer(ContextDictSerializer):
             "format": "application/json",
             "chars": body
         }
+
+    def _get_voice_data(self, obj):
+        if not 'voices_ii' in obj:
+            return None
+
+        id_list = ",".join([str(x) for x in obj['voices_ii']])
+        connection = pysolr.Solr(settings.SOLR['SERVER'])
+        fq = ["type:voice", "{!terms f=pk}"+id_list]
+        sort = "sort_order_i asc"
+        voice_list = connection.search("*:*", fq=fq, sort=sort, rows=100)
+
+        if voice_list.hits == 0:
+            return []
+
+        serialized = AnnotationVoiceSerializer(voice_list.docs, many=True).data
+        return serialized
+
 
 
 class AnnotationListSerializer(ContextDictSerializer):
