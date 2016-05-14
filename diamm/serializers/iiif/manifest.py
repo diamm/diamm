20afa12c9fe8fd7318b9ff39dabfc1cf7ee86a0a
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-05-14 15:26:41 -0400

    New: Use Ranges and Services for item information
    Previously, information on the contents of a page were given using an AnnotationList. This was problematic because technically structured data was not allowed in an OpenAnnotation annotation.
    
    This commit changes this behaviour by introducing sequences and ranges to deliver page-level contents, and a third-party service block to carry extra information on the contents of the page.
    
    Closes #17

--- diamm/serializers/iiif/manifest.py
+++ diamm/serializers/iiif/manifest.py
@@ -26,6 +26,139 @@ METADATA_MAPPING = {
 }
 
 
+class ServiceSerializer(ContextDictSerializer):
+    service = StaticField(
+        label="@context",
+        value="https://www.diamm.ac.uk/services/item"
+    )
+    id = serpy.MethodField(
+        label="@id"
+    )
+    type = StaticField(
+        value="Item"
+    )
+
+    composers = serpy.MethodField()
+    voices = serpy.MethodField()
+    folios = serpy.MethodField()
+
+    def get_id(self, obj):
+        return reverse('source-item-detail',
+                       kwargs={"source_id": obj['source_i'],
+                               "item_id": obj['pk']},
+                       request=self.context['request'])
+
+    def get_composers(self, obj):
+        composers = []
+        for composer in obj['composers_ssni']:
+            c = {}
+            name, pk, uncertain = composer.split("|")
+            c['name'] = name
+
+            if pk:
+                composer_url = reverse("person-detail",
+                                       kwargs={"pk": pk},
+                                       request=self.context['request'])
+                c["@id"] = composer_url
+
+            if uncertain and uncertain == "True":
+                c['uncertain'] = True
+            else:
+                c['uncertain'] = False
+
+            composers.append(c)
+        return composers
+
+    def get_voices(self, obj):
+        id_list = ",".join([str(x) for x in obj['voices_ii']])
+        connection = pysolr.Solr(settings.SOLR['SERVER'])
+        fq = ["type:voice", "{!terms f=pk}"+id_list]
+        sort = "sort_order_i asc"
+        voice_list = connection.search("*:*", fq=fq, sort=sort, rows=100)
+
+        if voice_list.hits == 0:
+            return []
+
+        voices = []
+        for voice in voice_list.docs:
+            v = {}
+
+            v['voice_type'] = voice.get('voice_type_s')
+            v['voice_text'] = voice.get('voice_text_s')
+            v['languages'] = voice.get('languages_ss')  # an array of languages
+            v['clef'] = voice.get('clef_s')
+            v['mensuration_sign'] = voice.get('mensuration_s')
+            v['mensuration_text'] = voice.get('mensuration_text_s')
+
+            v = {key: value for key, value in v.items() if value}
+            voices.append(v)
+
+        return voices
+
+    def get_folios(self, obj):
+        if not 'pages_ii' in obj:
+            return {}
+
+        folios = {}
+
+        folios['start'] = {
+            'label': obj['folio_start_s'],
+            '@id': reverse('source-canvas-detail',
+                           kwargs={"source_id": obj['source_i'],
+                                   "page_id": obj['pages_ii'][0]},
+                           request=self.context['request'])
+        }
+
+        folios['end'] = {
+            'label': obj['folio_end_s'],
+            '@id': reverse('source-canvas-detail',
+                           kwargs={'source_id': obj['source_i'],
+                                   "page_id": obj['pages_ii'][-1]},
+                           request=self.context['request'])
+        }
+
+        return folios
+
+
+class StructureSerializer(ContextDictSerializer):
+    canvases = serpy.MethodField()
+    id = serpy.MethodField(
+        label="@id"
+    )
+    type = StaticField(
+        label="@type",
+        value="sc:Range"
+    )
+    label = serpy.StrField(
+        attr='composition_s'
+    )
+
+    service = serpy.MethodField()
+
+    def get_canvases(self, obj):
+        if not obj.get('pages_ii'):
+            return []
+
+        canvases = []
+        for p in obj['pages_ii']:
+            canvas_id = reverse("source-canvas-detail",
+                                kwargs={"source_id": obj['source_i'],
+                                "page_id": p},
+                                request=self.context['request'])
+            canvases.append(canvas_id)
+
+        return canvases
+
+    def get_id(self, obj):
+        return reverse('source-range-detail',
+                       kwargs={"source_id": obj['source_i'],
+                               "item_id": obj['pk']},
+                       request=self.context['request'])
+
+    def get_service(self, obj):
+        return ServiceSerializer(obj, context={"request": self.context['request']}).data
+
+
 class ImageSerializer(ContextDictSerializer):
     type = StaticField(
         label="@type",
@@ -77,10 +210,6 @@ class CanvasSerializer(ContextDictSerializer):
         attr="numeration_s"
     )
     images = serpy.MethodField()
-    other_content = serpy.MethodField(
-        label="otherContent",
-        required=False
-    )
 
     def get_id(self, obj):
         return reverse(
@@ -102,19 +231,6 @@ class CanvasSerializer(ContextDictSerializer):
         }
         imgs_data = [ImageSerializer(i, context=context).data for i in imgs]
         return imgs_data
-
-    def get_other_content(self, obj):
-        if 'items_ii' not in obj:
-            return None
-        # returns a link to an annotation list for this particular canvas.
-        other_content_url = reverse("source-canvas-annotations",
-                                kwargs={"source_id": obj["source_i"],
-                                        "page_id": obj["pk"]},
-                                request=self.context['request'])
-        return [{
-            "@id": other_content_url,
-            "@type": "sc:AnnotationList"
-        }]
 
 
 class SourceManifestSerializer(ContextDictSerializer):
@@ -138,6 +254,7 @@ class SourceManifestSerializer(ContextDictSerializer):
     )
     related = serpy.MethodField()
     sequences = serpy.MethodField()
+    structures = serpy.MethodField()
     thumbnail = serpy.MethodField()
 
     def get_id(self, obj):
@@ -229,3 +346,17 @@ class SourceManifestSerializer(ContextDictSerializer):
                     "profile": "http://iiif.io/api/image/2/level1.json"
                 }
             }
+
+    def get_structures(self, obj):
+        conn = pysolr.Solr(settings.SOLR['SERVER'])
+
+        structure_query = {
+            "fq": ["type:item", "source_i:{0}".format(obj['pk'])],
+            "sort": "folio_start_ans asc",
+            "rows": 10000,
+        }
+        structure_res = conn.search("*:*", **structure_query)
+        structures = [StructureSerializer(s, context={"request": self.context["request"]}).data
+                      for s in structure_res.docs]
+
+        return structures
