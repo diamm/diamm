37f5432079109bd7cb9b8a35f2babf8d34eb7c19
Arielle Goldman <arielle745@hotmail.com>
2016-06-03 16:39:39 -0400

    Fixed: name styling
    'type' was being used to refer to organization type and content type.
    Also converted the serializer to serpy

--- diamm/serializers/website/organization.py
+++ diamm/serializers/website/organization.py
@@ -1,20 +1,42 @@
-from rest_framework import serializers
-from diamm.models.data.organization import Organization
+import serpy
+from diamm.serializers.serializers import ContextSerializer
+from rest_framework.reverse import reverse
 
 
-class OrganizationListSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Organization
-        fields = ('url', 'name')
+class OrganizationListSerializer(ContextSerializer):
+    pk = serpy.IntField()
+    url = serpy.MethodField()
+    name = serpy.StrField()
+
+    def get_url(self, obj):
+        return reverse('organization-detail',
+                       kwargs={"pk": obj.pk},
+                       request=self.context['request'])
 
 
-class OrganizationDetailSerializer(serializers.HyperlinkedModelSerializer):
-    type = serializers.SerializerMethodField()
-    pk = serializers.IntegerField()
+class OrganizationDetailSerializer(ContextSerializer):
+    pk = serpy.IntField()
+    url = serpy.MethodField()
+    name = serpy.StrField()
+    location = serpy.StrField()
+    note = serpy.StrField(
+        required=False
+    )
+    variant_names = serpy.StrField(
+        required=False
+    )
 
-    class Meta:
-        model = Organization
-        fields = ('url', 'name', 'type', 'pk')
+    #the name 'type' is needed for the content type but using organization.organization_type would be redundant
+    organization_type = serpy.StrField(
+        attr="type"
+    )
+
+    type = serpy.MethodField()
 
     def get_type(self, obj):
-        return self.Meta.model.__name__.lower()
+        return obj.__class__.__name__.lower()
+
+    def get_url(self, obj):
+        return reverse('organization-detail',
+                       kwargs={"pk": obj.pk},
+                       request=self.context['request'])
