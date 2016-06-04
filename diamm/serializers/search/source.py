2a85f57eb8efb79e5cf0d316041c7871f25c8bc8
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-06-04 15:36:25 -0400

    Refactor: Exclude project sets from source sets
    Project sets are usually internal to DIAMM and should not be advertised as part of the public interface.

--- diamm/serializers/search/source.py
+++ diamm/serializers/search/source.py
@@ -107,17 +107,20 @@ class SourceSearchSerializer(serpy.Serializer):
             return []
 
     def get_sets_ii(self, obj):
-        if obj.sets.count() > 0:
-            return list(obj.sets.values_list("pk", flat=True))
+        """
+            Indexes membership in all of the sets that are not project sets (type=7).
+        """
+        if obj.sets.exclude(type=7).count() > 0:
+            return list(obj.sets.exclude(type=7).values_list("pk", flat=True))
         else:
             return []
 
     def get_sets_ssni(self, obj):
         """
-            PK|Name for the sets this source belongs to.
+            PK|Name for the sets this source belongs to except project sets (type=7)
         """
-        if obj.sets.count() > 0:
-            sourcesets = obj.sets.values_list("pk", "cluster_shelfmark")
+        if obj.sets.exclude(type=7).count() > 0:
+            sourcesets = obj.sets.exclude(type=7).values_list("pk", "cluster_shelfmark")
             sets = ["{0}|{1}".format(sset[0], sset[1]) for sset in sourcesets]
             return sets
         else:
