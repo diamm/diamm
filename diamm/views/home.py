1fb9cdb61ec50c87a7987a1d138b2005052b2d38
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-06-05 20:52:18 -0400

    New: Add some counts to the front page
    This commit changes the home view to add a few interesting counts to help drive the info on the front page.

--- diamm/views/home.py
+++ diamm/views/home.py
@@ -1,18 +1,31 @@
 from rest_framework import views
 from rest_framework.response import Response
 from diamm.models.site.story import Story
+from diamm.models.data.source import Source
+from diamm.models.data.archive import Archive
 from diamm.serializers.website.story import StorySerializer
 
 
 class HomeView(views.APIView):
     template_name = "index.jinja2"
 
+    def _counts(self):
+        num_sources = Source.objects.count()
+        num_archives = Archive.objects.count()
+
+        return {
+            'num_sources': num_sources,
+            'num_archives': num_archives
+        }
+
+
     def get(self, request, *args, **kwargs):
         news_stories = Story.objects.order_by('created')[:3]
         news_stories_data = StorySerializer(news_stories,
                                             context={'request': request},
-                                            many=True)
+                                            many=True).data
 
         return Response({
-            'stories': news_stories_data.data
+            'stories': news_stories_data,
+            'counts': self._counts()
         })
