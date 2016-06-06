33a1f4d7d22b964e508c82f4dfab5e5c7b65c95d
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-06-05 20:48:55 -0400

    New: Signals for saving pages and images
    This commit adds a new signal for catching page and image saves.

--- diamm/signals/page_signals.py
+++ diamm/signals/page_signals.py
@@ -1,15 +1,19 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
-from diamm.models.data.image import
-from diamm.serializers.search.
+from diamm.models.data.image import Image
+from diamm.models.data.page import Page
+from diamm.serializers.search.page import PageSearchSerializer
 from diamm.helpers.solr_helpers import solr_delete, solr_index
 
 
+# When an image record is saved we need to re-index the entire page since images are indexed as children of pages.
 @receiver(post_save, sender=Image)
 def index_image(sender, instance, created, **kwargs):
-    solr_index(ArchiveSearchSerializer, instance)
+    solr_delete(instance)
+    if instance.page:
+        solr_index(PageSearchSerializer, instance.page)
 
 
-@receiver(post_delete, sender=Image)
-def delete_archive(sender, instance, **kwargs):
-    solr_delete(ArchiveSearchSerializer, instance)
+@receiver(post_save, sender=Page)
+def index_page(sender, instance, created, **kwargs):
+    solr_index(PageSearchSerializer, instance)
