bead7729a4e562dc03691e97dcdda42be248827f
Arielle Goldman <arielle745@hotmail.com>
2016-06-06 09:50:36 -0400

    Fixed: eliminated unnecessary function
    Removed the contribution sorter that was specifically for sources and use the same function for all the models now.

--- diamm/templatetags/contribution_sort.py
+++ diamm/templatetags/contribution_sort.py
@@ -1,5 +1,4 @@
 from django_jinja import library
-from django.contrib.contenttypes.models import ContentType
 from diamm.models.site.contribution import Contribution
 
 
@@ -11,20 +10,5 @@ def contribution_sort(content_name):
     for contribution in contributions:
         if contribution.record.__str__() == content_name:
             contributionlist.append(contribution)
-
     return contributionlist
-
-
-@library.global_function
-def contribution_source_sort(content_name):
-    contributionlist = []
-    source_content_type = ContentType.objects.get(app_label="diamm_data", model="source")
-    contributions = Contribution.objects.filter(content_type=source_content_type)
-
-    for contribution in contributions:
-        if contribution.record.display_name.__str__() == content_name:
-            contributionlist.append(contribution)
-
-    return contributionlist
-
 
