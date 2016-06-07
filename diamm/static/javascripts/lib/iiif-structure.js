b851546ed6a38a1420e8beb2dc6deb776b505f13
Justin Bell <justin.bell@mail.mcgill.ca>
2016-06-07 15:36:12 -0400

    WIP: diva plugin refactoring and features
    Added composition to diva structure plugin
    
    factored out some components of the displayItem function to make it more
    readable
    
    Refs #47

--- diamm/static/javascripts/lib/iiif-structure.js
+++ diamm/static/javascripts/lib/iiif-structure.js
@@ -6,26 +6,6 @@ window.divaPlugins.push((function ()
     var structures = [];
     var items = [];
 
-    var itemsDiv = document.getElementById("image-item-listing");
-    var addVisibilityToggle = function (div)
-    {
-        div.style.display = 'none';
-        function toggleVisibility ()
-        {
-            if (div.style.display === 'block')
-            {
-                div.style.display = 'none';
-            }
-            else
-            {
-                div.style.display = 'block';
-            }
-        }
-
-        div.parentElement.onclick = toggleVisibility;
-        div.parentElement.style.cursor = 'pointer';
-    };
-
     // Adapted from Underscore.js
     // Returns a function that will only be trigerred 'wait' milliseconds
     // after the last time it was called.
@@ -45,88 +25,159 @@ window.divaPlugins.push((function ()
         };
     }
 
+    var itemsDiv = document.getElementById("image-item-listing");
+    var addVisibilityToggle = function (element)
+    {
+        element.style.display = 'none';
+        function toggleVisibility ()
+        {
+            if (element.style.display === 'block')
+            {
+                element.style.display = 'none';
+            }
+            else
+            {
+                element.style.display = 'block';
+            }
+        }
+
+        element.parentElement.onclick = toggleVisibility;
+        element.parentElement.style.cursor = 'pointer';
+    };
+
     var populateStructures = function (manifest)
     {
-        var displayItem = function (item, serviceLabel)
+        function getCompositionItem (composition)
         {
-            var itemDiv = document.createElement("div");
-            var h3 = document.createElement("h3"); 
-            var t = document.createTextNode(serviceLabel);
-            h3.appendChild(t);
-            var itemDetailsDiv = document.createElement("div");
-            var ul = document.createElement("ul");
+            var li, t, a;
+            li = document.createElement("li");
+            t = document.createTextNode("Composition: ");
+            li.appendChild(t);
+            a = document.createElement("a");
+            t = document.createTextNode(composition.title);
+            a.setAttribute('href', composition['@id']);
+            a.appendChild(t);
+            li.appendChild(a);
 
-            // composer
-            var li = document.createElement("li");
+            return li;
+        }
+
+        function getComposersItem (composers)
+        {
+            var li, t, a;
+            li = document.createElement("li");
             t = document.createTextNode("Composer: ");
             li.appendChild(t);
-            var a;
-            for (var i = 0, clen = item[0].composers.length; i < clen; i++)
+            for (var i = 0, clen = composers.length; i < clen; i++)
             {
-                if (item[0].composers[i]['@id'])
+                if (composers[i]['@id'])
                 {
                     a = document.createElement("a");
-                    t = document.createTextNode(item[0].composers[i].name + " ");
-                    a.setAttribute('href', item[0].composers[i]['@id']);
+                    t = document.createTextNode(composers[i].name + " ");
+                    a.setAttribute('href', composers[i]['@id']);
                     a.appendChild(t);
                     li.appendChild(a);
                 }
                 else
                 {
-                    t = document.createTextNode(item[0].composers[i].name + " ");
+                    t = document.createTextNode(composers[i].name + " ");
                     li.appendChild(t);
                 }
 
-                if (item[0].composers[i].uncertain)
+                if (composers[i].uncertain)
                 {
                     t = document.createTextNode("? ");
                     li.appendChild(t);
                 }
             }
-            ul.appendChild(li);
 
-            // folio
+            return li;
+        }
+
+        function getFolioItem (folio)
+        {
+            var li;
             li = document.createElement("li");
-            if (item[0].folios.start.label === item[0].folios.end.label)
+            if (folio.start.label === folio.end.label)
             {
-                t = document.createTextNode("Folio: " + item[0].folios.start.label);
+                t = document.createTextNode("Folio: " + folio.start.label);
             }
             else
             {
-                t = document.createTextNode("Folio: " + item[0].folios.start.label + "-" + item[0].folios.end.label);
+                t = document.createTextNode("Folio: " + folio.start.label + "-" + folio.end.label);
 
             }
             li.appendChild(t);
-            ul.appendChild(li);
-            itemDetailsDiv.appendChild(ul);
+            return li;
+        }
 
-            // voices
-            var voices = item[0].voices, ul2;
+        function getVoicesItem (voices)
+        {
+            var ul, ul2, li, li2, div, t;
+            li = document.createElement("li");
+            t = document.createTextNode("Voices");
+            div = document.createElement("div");
+            div.appendChild(t);
+            li.appendChild(div);
+
             for (i = 0, vlen = voices.length; i < vlen; i++)
             {
+                ul = document.createElement("ul");
                 t = document.createTextNode(voices[i].voice_text);
-                li = document.createElement("li");
-                li.appendChild(t);
-                ul.appendChild(li);
-
-                if (voices[i].voice_type !== "no designation")
-                {
-                    ul2 = document.createElement("ul");
-                    t = document.createTextNode("Type: " + voices[i].voice_type);
-                    li = document.createElement("li");
-                    li.appendChild(t);
-                    ul2.appendChild(li);
-                }
+                li2 = document.createElement("li");
+                li2.appendChild(t);
+                ul.appendChild(li2);
 
                 ul2 = document.createElement("ul");
+                if (voices[i].voice_type !== "no designation")
+                {
+                    t = document.createTextNode("Type: " + voices[i].voice_type);
+                    li2 = document.createElement("li");
+                    li2.appendChild(t);
+                    ul2.appendChild(li2);
+                }
+
+                li2 = document.createElement("li");
                 t = document.createTextNode("Language: " + voices[i].languages[0]);
-                li = document.createElement("li");
-                li.appendChild(t);
-                ul2.appendChild(li);
+                li2.appendChild(t);
+                ul2.appendChild(li2);
                 ul.appendChild(ul2);
             }
 
+            div.appendChild(ul);
+            addVisibilityToggle(ul);
+            li.appendChild(div);
+            return li;
+        }
+
+        var displayItem = function (item, serviceLabel)
+        {
+            var itemDiv, itemDetailsDiv, h3, t, ul, li;
+            itemDiv = document.createElement("div");
+            h3 = document.createElement("h3");
+            t = document.createTextNode(serviceLabel);
+            h3.appendChild(t);
+            itemDetailsDiv = document.createElement("div");
+            ul = document.createElement("ul");
+
+            // composition
+            li = getCompositionItem(item[0].composition);
+            ul.appendChild(li);
+
+            // composer
+            li = getComposersItem(item[0].composers);
+            ul.appendChild(li);
+
+            // folio
+            li = getFolioItem (item[0].folios);
+            ul.appendChild(li);
+
+            // voices
+            li = getVoicesItem(item[0].voices);
+            ul.appendChild(li);
+
             itemDiv.appendChild(h3);
+            itemDetailsDiv.appendChild(ul);
             itemDiv.appendChild(itemDetailsDiv);
             itemsDiv.appendChild(itemDiv);
         };
