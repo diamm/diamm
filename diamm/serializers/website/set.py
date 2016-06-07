187931cab3cda03e5570a1d831915fb3021c6b73
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-06-07 01:38:42 -0400

    Fixed: Add more info to set serializer

--- diamm/serializers/website/set.py
+++ diamm/serializers/website/set.py
@@ -3,12 +3,36 @@ from rest_framework.reverse import reverse
 from diamm.serializers.serializers import ContextSerializer
 
 
+class SetSourceSerializer(ContextSerializer):
+    url = serpy.MethodField()
+    shelfmark = serpy.StrField()
+    display_name = serpy.StrField(
+        required=False
+    )
+
+    def get_url(self, obj):
+        return reverse('source-detail',
+                       kwargs={"pk": obj.pk},
+                       request=self.context['request'])
+
+
 class SetDetailSerializer(ContextSerializer):
     pk = serpy.IntField()
     url = serpy.MethodField()
-    type = serpy.StrField()
+    type = serpy.StrField(
+        attr='set_type'
+    )
+    cluster_shelfmark = serpy.StrField()
+    sources = serpy.MethodField()
 
     def get_url(self, obj):
         return reverse('set-detail',
                        kwargs={"pk": obj.pk},
                        request=self.context['request'])
+
+    def get_sources(self, obj):
+        if obj.sources:
+            return [SetSourceSerializer(o, context={'request': self.context['request']}).data
+                    for o in obj.sources.all()]
+        else:
+            return None
