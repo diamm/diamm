3ef31772acdff353ff7cedcffdfbacba3fa40f95
Arielle Goldman <arielle745@hotmail.com>
2016-06-08 12:06:04 -0400

    Rewrote composition serializer to serpy
    closes #71

--- diamm/serializers/website/composition.py
+++ diamm/serializers/website/composition.py
@@ -1,21 +1,32 @@
-from rest_framework import serializers
-from diamm.models.data.composition import Composition
-from diamm.models.data.composition_composer import CompositionComposer
-from diamm.models.data.item import Item
+import serpy
+from rest_framework.reverse import reverse
+from diamm.serializers.serializers import ContextSerializer
 
 
-class CompositionSourceSerializer(serializers.ModelSerializer):
-    url = serializers.HyperlinkedRelatedField(view_name='source-detail',
-                                              source='source.id',
-                                              read_only=True)
-    has_images = serializers.SerializerMethodField()
+class CompositionContributionSerializer(ContextSerializer):
+    contributor = serpy.StrField(
+        attr="contributor.username"
+    )
+    summary = serpy.StrField()
+    updated = serpy.StrField()
 
-    display_name = serializers.ReadOnlyField(source='source.display_name')
-    public_images = serializers.ReadOnlyField(source='source.public_images')
 
-    class Meta:
-        model = Item
-        fields = ('url', 'display_name', 'public_images', 'has_images')
+class CompositionSourceSerializer(ContextSerializer):
+    url = serpy.MethodField()
+    display_name = serpy.StrField(
+        attr="source.display_name"
+    )
+
+    has_images = serpy.MethodField()
+
+    public_images = serpy.BoolField(
+        attr="source.public_images"
+    )
+
+    def get_url(self, obj):
+        return reverse('source-detail',
+                       kwargs={"pk": obj.source.pk},
+                       request=self.context['request'])
 
     def get_has_images(self,obj):
         if obj.pages.count() > 0:
@@ -23,34 +34,62 @@ class CompositionSourceSerializer(serializers.ModelSerializer):
         return False
 
 
-class CompositionComposerSerializer(serializers.ModelSerializer):
-    url = serializers.HyperlinkedRelatedField(view_name="person-detail",
-                                              source="composer.id",
-                                              read_only=True)
-    full_name = serializers.ReadOnlyField(source="composer.full_name")
+class CompositionComposerSerializer(ContextSerializer):
+    url = serpy.MethodField()
+    full_name = serpy.StrField(
+        attr="composer.full_name"
+    )
+    uncertain = serpy.BoolField()
+    notes = serpy.StrField()
 
-    class Meta:
-        model = CompositionComposer
-        fields = ('url', 'full_name', 'uncertain', 'notes')
-
-
-class CompositionListSerializer(serializers.HyperlinkedModelSerializer):
-    composers = CompositionComposerSerializer(many=True)
-
-    class Meta:
-        model = Composition
-        fields = ('url', 'title', 'composers')
+    def get_url(self, obj):
+        return reverse('person-detail',
+                kwargs={"pk": obj.composer.pk},
+                request=self.context['request'])
 
 
-class CompositionDetailSerializer(serializers.HyperlinkedModelSerializer):
-    composers = CompositionComposerSerializer(many=True)
-    sources = CompositionSourceSerializer(many=True)
-    type = serializers.SerializerMethodField()
+class CompositionListSerializer(ContextSerializer):
+    composers = serpy.MethodField()
+    title = serpy.StrField()
 
-    class Meta:
-        model = Composition
-        fields = ('url', 'title', 'composers', 'sources', 'pk', 'type')
+    def get_composers(self, obj):
+        if obj.composers:
+            return [CompositionComposerSerializer(o, context={"request": self.context['request']}).data
+                    for o in obj.composers.all()]
+
+
+class CompositionDetailSerializer(ContextSerializer):
+    composers = serpy.MethodField()
+    sources = serpy.MethodField()
+    type = serpy.MethodField()
+    pk = serpy.IntField()
+    url = serpy.MethodField()
+    title = serpy.StrField()
+    contributors = serpy.MethodField()
+
+    def get_url(self, obj):
+        return reverse('composition-detail',
+                       kwargs={"pk": obj.pk},
+                       request=self.context['request'])
 
     def get_type(self, obj):
-        return self.Meta.model.__name__.lower()
+        return obj.__class__.__name__.lower()
 
+    def get_sources(self, obj):
+        if obj.sources:
+            return [CompositionSourceSerializer(o, context={'request': self.context['request']}).data
+                    for o in obj.sources.all()]
+        else:
+            return None
+
+    def get_composers(self, obj):
+        if obj.composers:
+            return [CompositionComposerSerializer(o, context={"request": self.context['request']}).data
+                    for o in obj.composers.all()]
+        else:
+            return None
+
+    def get_contributors(self, obj):
+        if obj.contributions.count() > 0:
+            return CompositionContributionSerializer(obj.contributions.filter(completed=True),
+                                                context={"request": self.context['request']}, many=True).data
