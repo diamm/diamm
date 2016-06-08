8bebcd847ec50e8d62810f0a529b59b274a2c423
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-06-08 17:53:14 -0400

    Fixed: Pass X-IIIF-ID from original request
    Instead of re-writing it, pass it along from the proxy.

--- diamm/views/website/image.py
+++ diamm/views/website/image.py
@@ -44,10 +44,11 @@ def image_serve(request, pk, region=None, size=None, rotation=None, *args, **kwa
         location = "{0}/{1}/{2}/{3}/default.jpg".format(location, region, size, rotation)
 
     diamm = request.META.get('HTTP_X_DIAMM')
+    iiif_id = request.META.get('HTTP_X_IIIF_ID')
 
     r = requests.get(location, stream=True, headers={'referer': referer,
                                                      'X-DIAMM': diamm,
-                                                     'X-IIIF-ID': request.build_absolute_uri()}, verify=False)
+                                                     'X-IIIF-ID': iiif_id}, verify=False)
 
     # If the response was a 200 (success) pass this along.
     if 200 <= r.status_code < 300:
