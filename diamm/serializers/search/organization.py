db5284087099da1fdb9f045ed84a071b55887d4a
Arielle Goldman <arielle745@hotmail.com>
2016-06-21 10:45:13 -0400

    Fixed: indexing bug
    closes #95

--- diamm/serializers/search/organization.py
+++ diamm/serializers/search/organization.py
@@ -6,10 +6,8 @@ class OrganizationSearchSerializer(serpy.Serializer):
     type = serpy.MethodField()
     pk = serpy.IntField()
 
-    location_s = serpy.StrField(
-        attr="location.name",
-        required=False
-    )
+    location_s = serpy.MethodField()
+
     name_s = serpy.StrField(
         attr="name",
     )
@@ -20,3 +18,9 @@ class OrganizationSearchSerializer(serpy.Serializer):
 
     def get_type(self, obj):
         return obj.__class__.__name__.lower()
+
+    def get_location_s(self, obj):
+        if obj.location:
+            return obj.location.name
+        else:
+            return None
