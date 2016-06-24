600ee0ffeb3026226ce176fae6f48b220871950d
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-06-24 00:33:06 -0400

    New: Forms for log in and account creation

---
+++ diamm/forms/create_account_form.py
@@ -0,0 +1,87 @@
+from django import forms
+from django.contrib.auth.password_validation import validate_password
+from django.utils.safestring import mark_safe
+from diamm.models.diamm_user import CustomUserModel
+
+
+class CreateAccountForm(forms.Form):
+    first_name = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={"class": "u-full-width"}
+        )
+    )
+    last_name = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={"class": "u-full-width"}
+        )
+    )
+    affiliation = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={"class": "u-full-width"}
+        )
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(
+            attrs={"class": "u-full-width"}
+        )
+    )
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={"class": "u-full-width"}
+        ),
+    )
+
+    confirm_password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={"class": "u-full-width"}
+        )
+    )
+
+    def clean(self):
+        """
+            Validates that the passwords are present, that they match, and that the passwords
+            meet minimum security standards.
+
+        :return: None; will raise validation error if a condition is not met.
+        """
+        cleaned_data = super(CreateAccountForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        email = cleaned_data.get('email')
+
+        email_exists = CustomUserModel.objects.filter(email=email).exists()
+        if email_exists:
+            email_err = """
+                A user with the e-mail address {0} already exists. Try <a href='/recover'>recovering your password</a>.
+            """
+            err = forms.ValidationError(mark_safe(email_err.format(email)))
+            raise err
+
+        if not password:
+            err = forms.ValidationError('You must supply a password')
+            self.add_error('password', err)
+            raise err
+
+        if not confirm_password:
+            err = forms.ValidationError('You must re-type your password to confirm')
+            self.add_error('confirm_password', err)
+            raise err
+
+        if password != confirm_password:
+            err = forms.ValidationError('Your passwords did not match')
+            self.add_error('password', err)
+            self.add_error('confirm_password', err)
+            raise err
+
+        try:
+            validate_password(password)
+        except forms.ValidationError as err:
+            self.add_error('password', err)
+            raise err
