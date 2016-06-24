121986403687cf3b200c442727775876a396ebb7
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-06-24 00:33:53 -0400

    Fixed: Site migration uses new user model

--- diamm/management/helpers/migrate_users.py
+++ diamm/management/helpers/migrate_users.py
@@ -1,16 +1,17 @@
 from diamm.models.migrate.legacy_diammuser import LegacyDiammUser
 import uuid
-from django.contrib.auth.models import User
-from diamm.models.diamm_user import DIAMMUser
+# from django.contrib.auth.models import User
+from diamm.models.diamm_user import CustomUserModel
 import blessings
 
 term = blessings.Terminal()
 
+
 def empty_users():
     print(term.magenta("\tEmptying user table"))
     # NB: Don't empty PK 1!
-    DIAMMUser.objects.all().delete()
-    User.objects.exclude(username="ahankins").all().delete()
+    CustomUserModel.objects.all().delete()
+    # User.objects.exclude(username="ahankins").all().delete()
 
 
 def migrate_user(entry):
@@ -21,21 +22,15 @@ def migrate_user(entry):
     last_name = nm[-1]
     first_names = " ".join(nm[:-1])
     d = {
-        "username": entry.username,
+        "legacy_username": entry.username,
         "last_name": last_name,
         "first_name": first_names,
         "email": entry.email,
+        "affiliation": entry.affiliation,
         "password": str(uuid.uuid4())  # set a random password. This will be changed on first login.
     }
-    u = User.objects.create_user(**d)
+    u = CustomUserModel.objects.create_user(**d)
     u.save()
-
-    profile = {
-        "user": u,
-        "affiliation": entry.affiliation
-    }
-    du = DIAMMUser(**profile)
-    du.save()
 
 
 def migrate():
