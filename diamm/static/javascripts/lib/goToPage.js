b9af9f344a6d55516cbc7f25719c9de401a2e928
Justin Bell <justin.bell@mail.mcgill.ca>
2016-06-28 12:58:30 -0400

    removed a console.log

--- diamm/static/javascripts/lib/goToPage.js
+++ diamm/static/javascripts/lib/goToPage.js
@@ -9,7 +9,6 @@ var goToPage = function()
             newActiveTabPanel.classList.add("front");
 
             var diva_instance = $('#diva-wrapper').data('diva');
-            console.log(diva_instance);
             diva_instance.gotoPageByName(pageName);
         }
     }
