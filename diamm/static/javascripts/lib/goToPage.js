ccc2d9b007472be8cf672603c0eee30092df2197
Justin Bell <justin.bell@mail.mcgill.ca>
2016-06-28 12:16:47 -0400

    Refactor: removes onClick from html

--- diamm/static/javascripts/lib/goToPage.js
+++ diamm/static/javascripts/lib/goToPage.js
@@ -1,11 +1,24 @@
-var goToPage = function (pageName)
+var goToPage = function() 
 {
-    activeTabPanel = document.querySelector(".front");
-    activeTabPanel.classList.remove("front");
-    newActiveTabPanel = document.querySelector("#images");
-    newActiveTabPanel.classList.add("front");
+    var _onFolioClick = function (pageName)
+    {
+        return function() {
+            activeTabPanel = document.querySelector(".front");
+            activeTabPanel.classList.remove("front");
+            newActiveTabPanel = document.querySelector("#images");
+            newActiveTabPanel.classList.add("front");
 
-    var diva_instance = $('#diva-wrapper').data('diva');
-    console.log(diva_instance);
-    diva_instance.gotoPageByName(pageName);
+            var diva_instance = $('#diva-wrapper').data('diva');
+            console.log(diva_instance);
+            diva_instance.gotoPageByName(pageName);
+        }
+    }
+
+    var folio_links = document.querySelectorAll('.folio-link');
+    for (var i=0, len=folio_links.length; i < len; i++) 
+    {
+        page = folio_links[i].dataset.page;
+        folio_links[i].addEventListener("click", _onFolioClick(page) )
+    }
+
 }
