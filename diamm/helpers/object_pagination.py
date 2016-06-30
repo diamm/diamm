b7067c83fad9fbd0bc4c8bb19ea8747e1414ebfd
Justin Bell <justin.bell@mail.mcgill.ca>
2016-06-30 14:44:10 -0400

    Adds a class that paginates objects returned from postgresdb and adds it to the City view. Refs #96

---
+++ diamm/helpers/object_pagination.py
@@ -0,0 +1,30 @@
+from collections import OrderedDict
+import re
+
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+
+class Object_Pagination(PageNumberPagination):
+    def replace_page_query(self, url, page_num):
+        regex = "(?P<prefix>&|\?)page=[0-9]+"
+        if re.search(regex, url):
+            return re.sub(regex, "\g<prefix>page=%d" % page_num, url)
+        else:
+            prefix = "&" if url.count("?") else "?"
+            return ("%s%spage=%d" % (url, prefix, page_num))
+
+    def get_paginated_response(self, data):
+        base_url = self.request.build_absolute_uri()
+        pages = dict((i, self.replace_page_query(base_url, i)) for i in range(1, self.page.paginator.num_pages + 1))
+
+        return Response(OrderedDict([
+            ('results', data),
+            ('count', self.page.paginator.count),
+            ('pagination', {
+                'current_page': self.page.number,
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link(),
+                'num_pages': self.page.paginator.num_pages,
+                'pages': pages
+            })
+        ]))
--- diamm/views/website/city.py
+++ diamm/views/website/city.py
@@ -3,6 +3,7 @@ from rest_framework import renderers
 from diamm.models.data.geographic_area import GeographicArea
 from diamm.renderers.html_renderer import HTMLRenderer
 from diamm.serializers.website.city import CityDetailSerializer, CityListSerializer
+from diamm.helpers.object_pagination import Object_Pagination
 
 
 class CityList(generics.ListAPIView):
@@ -10,7 +11,7 @@ class CityList(generics.ListAPIView):
     renderer_classes = (HTMLRenderer, renderers.JSONRenderer)
     serializer_class = CityListSerializer
     queryset = GeographicArea.objects.filter(type=GeographicArea.CITY)
-
+    pagination_class = Object_Pagination
 
 class CityDetail(generics.RetrieveAPIView):
     template_name = "website/city/city_detail.jinja2"
