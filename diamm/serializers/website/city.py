edf1aaa1e8b97c05d80a3e6ab50442a625d6eca7
Justin Bell <justin.bell@mail.mcgill.ca>
2016-07-05 14:52:13 -0400

    Adds sources to cityDetail serializer and template

--- diamm/serializers/website/city.py
+++ diamm/serializers/website/city.py
@@ -1,4 +1,8 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
+import pysolr
+
+from django.conf import settings
 from diamm.models.data.geographic_area import GeographicArea
 from diamm.models.data.archive import Archive
 
@@ -29,12 +33,31 @@ class CityListSerializer(serializers.ModelSerializer):
 class CityDetailSerializer(serializers.HyperlinkedModelSerializer):
     archives = ArchiveCitySerializer(many=True)
     country = CountryCitySerializer(
-        source="parent"
+        source='parent'
     )
+    sources = serializers.SerializerMethodField() 
 
     class Meta:
         model = GeographicArea
-        fields = ('url', 'name', 'archives', 'country')
+        fields = ('url', 'name', 'archives', 'country', 'sources')
         extra_kwargs = {
             'url': {'view_name': 'city-detail'}
         }
+
+    def get_sources(self, obj):
+        sources = list()
+        connection = pysolr.Solr(settings.SOLR['SERVER'])
+        fq = ['type:source', 'geographic_area_ii:' + str(obj.id)]
+        results = connection.search('*:*', fq=fq)
+        if results.hits > 0:
+            for doc in results.docs:
+                sources.append({
+                    'url': 
+                        reverse(
+                            'source-detail', 
+                            kwargs={'pk': doc['pk']},
+                            request=self.context['request']),
+                    'name': doc['display_name_s']
+                })
+        return sources 
+
