870de9e2d7a352f8e2af3257901fe416b014d862
Justin Bell <justin.bell@mail.mcgill.ca>
2016-07-07 15:57:27 -0400

    Refactor: simplified alphabetical browsing

--- diamm/helpers/object_pagination.py
+++ diamm/helpers/object_pagination.py
@@ -13,25 +13,16 @@ class ObjectPagination(PageNumberPagination):
             prefix = "&" if url.count("?") else "?"
             return ("%s%spage=%d" % (url, prefix, page_num))
 
-    def remove_queries(self, url):
-        regex = "((&|\?)page=[0-9]+)|((&|\?)l=[a-zA-Z]+)"
-        s = re.search(regex, url)
-        while s:
-            url = url[:s.start()] + url[s.end():]
-            s = re.search(regex, url)
-        url = re.sub("&", "?", url)
-        url += "&" if url.count("?") else "?"
-        return url
-
     def get_paginated_response(self, data):
         base_url = self.request.build_absolute_uri()
+        url = base_url[:base_url.find("?")]
         pages = dict((i, self.replace_page_query(base_url, i)) for i in range(1, self.page.paginator.num_pages + 1))
 
         return Response(OrderedDict([
             ('results', data),
             ('count', self.page.paginator.count),
+            ('url', url),
             ('pagination', {
-                'url': self.remove_queries(base_url),
                 'current_page': self.page.number,
                 'next': self.get_next_link(),
                 'previous': self.get_previous_link(),
