ec0d74468e8ec2d11a0a719f63b5ca01ee6de247
Justin Bell <justin.bell@mail.mcgill.ca>
2016-07-12 17:39:39 -0400

    WIP: converting DRF serializers to serpy

--- diamm/serializers/website/city.py
+++ diamm/serializers/website/city.py
@@ -1,40 +1,44 @@
-from rest_framework import serializers
+import serpy
+from rest_framework.reverse import reverse
+
 from diamm.models.data.geographic_area import GeographicArea
 from diamm.models.data.archive import Archive
+from diamm.serializers.serializers import ContextSerializer
 
 
-class CountryCitySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = GeographicArea
-        fields = ('url', 'name')
-        extra_kwargs = {
-            'url': {'view_name': 'country-detail'}
-        }
+class CountryCitySerializer(ContextSerializer):
+    url = serpy.MethodField()
+    name = serpy.StrField()
 
+    #TODO: find a way to factor this url method out in a more DRY way
+    def get_url(self, obj):
+        return self.context['request'].build_absolute_uri(reverse('country-detail', [obj.pk]))
 
-class ArchiveCitySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Archive
-        fields = ('url', 'name')
+class ArchiveCitySerializer(ContextSerializer):
+    url = serpy.MethodField()
+    name = serpy.StrField()
 
+    def get_url(self, obj):
+        return self.context['request'].build_absolute_uri(reverse('archive-detail', [obj.pk]))
 
-class CityListSerializer(serializers.ModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name='city-detail')
+class CityListSerializer(ContextSerializer):
+    url = serpy.MethodField()
+    name = serpy.StrField()
 
-    class Meta:
-        model = GeographicArea
-        fields = ('url', 'name')
+    def get_url(self, obj):
+        return self.context['request'].build_absolute_uri(reverse('city-detail', [obj.pk]))
 
+class CityDetailSerializer(ContextSerializer):
+    url = serpy.MethodField()
+    name = serpy.StrField()
+    archives = serpy.MethodField()
+    country = serpy.MethodField()
 
-class CityDetailSerializer(serializers.HyperlinkedModelSerializer):
-    archives = ArchiveCitySerializer(many=True)
-    country = CountryCitySerializer(
-        source="parent"
-    )
+    def get_archives(self, obj):
+        return ArchiveCitySerializer(obj.archives.all(), many=True, context=self.context).data
 
-    class Meta:
-        model = GeographicArea
-        fields = ('url', 'name', 'archives', 'country')
-        extra_kwargs = {
-            'url': {'view_name': 'city-detail'}
-        }
+    def get_country(self, obj):
+        return CountryCitySerializer(obj.parent, context=self.context).data
+
+    def get_url(self, obj):
+        return self.context['request'].build_absolute_uri(reverse('city-detail', [obj.pk]))
