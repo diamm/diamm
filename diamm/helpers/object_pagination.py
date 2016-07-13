e348fa84976ad8a8cbc16c149e45a84e755cd2d6
Justin Bell <justin.bell@mail.mcgill.ca>
2016-07-13 11:24:41 -0400

    Refactor: replace query_replace method with the rest_framework util

--- diamm/helpers/object_pagination.py
+++ diamm/helpers/object_pagination.py
@@ -2,21 +2,14 @@ import re
 from collections import OrderedDict
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.utils.urls import replace_query_param
 
 
 class ObjectPagination(PageNumberPagination):
-    def replace_page_query(self, url, page_num):
-        regex = "(?P<prefix>&|\?)page=[0-9]+"
-        if re.search(regex, url):
-            return re.sub(regex, "\g<prefix>page=%d" % page_num, url)
-        else:
-            prefix = "&" if url.count("?") else "?"
-            return ("%s%spage=%d" % (url, prefix, page_num))
-
     def get_paginated_response(self, data):
         base_url = self.request.build_absolute_uri()
         url = base_url[:base_url.find("?")]
-        pages = dict((i, self.replace_page_query(base_url, i)) for i in range(1, self.page.paginator.num_pages + 1))
+        pages = dict((i, replace_query_param(base_url, 'page', i)) for i in range(1, self.page.paginator.num_pages + 1))
 
         return Response(OrderedDict([
             ('results', data),
