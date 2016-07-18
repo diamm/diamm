660974bd657e0037b4f0d13c9c7a48938f41f7bf
Justin Bell <justin.bell@mail.mcgill.ca>
2016-07-18 15:54:16 -0400

    Converts story serializer to serpy

--- diamm/serializers/website/story.py
+++ diamm/serializers/website/story.py
@@ -1,7 +1,23 @@
-from rest_framework import serializers
-from diamm.models.site.story import Story
+import serpy
+from rest_framework.reverse import reverse
+from diamm.serializers.website.tag import TagSerializer
+from diamm.serializers.serializers import ContextSerializer
 
+class StorySerializer(ContextSerializer):
+    title = serpy.StrField()
+    body = serpy.StrField()
+    tags = serpy.MethodField()
+    created = serpy.StrField()
+    updated = serpy.StrField()
+    url = serpy.MethodField()
 
-class StorySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Story
+    def get_tags(self, obj):
+        return TagSerializer(
+            obj.tags.all(),
+            many=True,
+            context={'request': self.context['request']}).data
+
+    def get_url(self, obj):
+        return reverse("tag-detail",
+                kwargs={"pk": obj.pk},
+                request=self.context['request'])
