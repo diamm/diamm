cee0822295b8ef69ad94451fdc815e31461cc2fb
Justin Bell <justin.bell@mail.mcgill.ca>
2016-08-15 12:58:26 -0400

    Fix: goToPage makes sure the diva instance is ready before going to the folio

--- diamm/static/javascripts/lib/goToPage.js
+++ diamm/static/javascripts/lib/goToPage.js
@@ -1,18 +1,33 @@
-var Page = function (params) {
+var Page = function (divaInstance)
+{
     var goToPage = function(pageName)
     {
         activeTabPanel = document.querySelector('.front');
-        activeTabPanel.classList.remove('front');
+        if (activeTabPanel)
+            activeTabPanel.classList.remove('front');
         newActiveTabPanel = document.querySelector('#images');
         newActiveTabPanel.classList.add('front');
 
-        var diva_instance = $('#diva-wrapper').data('diva');
-        diva_instance.gotoPageByName(pageName);
+        divaInstance.gotoPageByName(pageName);
         // This is to make sure the diva viewer resizes on tab change
         // Will be a non-issue in diva 5
         window.dispatchEvent(new Event('resize'));
     }
 
-    if (params['folio'])
-        goToPage(params['folio']);
+    return function (params) {
+        if (params['folio'])
+        {
+            if (!divaInstance.isReady())
+            {
+                var handle = diva.Events.subscribe(
+                    'ViewerDidLoad',
+                    function ()
+                    {
+                        goToPage(params['folio']);
+                        diva.Events.unsubscribe(handle);
+                    });
+            }
+            goToPage(params['folio']);
+        }
+    };
 }
