fbab354d2450af47b74ee47b0d5437642b678f1d
Justin Bell <justin.bell@mail.mcgill.ca>
2016-08-15 10:56:25 -0400

    New: use framgent identifier to navigate source detail
    Previously, the source detail page was:
    - using stateful javascript to navigate between tabs
    - adding an onClick function to each inventory link to navigate to
      folio pages
    
    Now there's an event listener on the fragment identifier (the bit of url
    after the #) which is initialized with callbacks. This has the added
    benefits of:
    - having urls that will take the user directly to the part of
    the resource she wants to view
    - being extensible to callbacks beyond Tabs and Folio

--- diamm/static/javascripts/lib/goToPage.js
+++ diamm/static/javascripts/lib/goToPage.js
@@ -1,27 +1,18 @@
-var goToPage = function()
-{
-    var _onFolioClick = function (pageName)
+var Page = function (params) {
+    var goToPage = function(pageName)
     {
-        return function()
-        {
-            activeTabPanel = document.querySelector(".front");
-            activeTabPanel.classList.remove("front");
-            newActiveTabPanel = document.querySelector("#images");
-            newActiveTabPanel.classList.add("front");
+        activeTabPanel = document.querySelector('.front');
+        activeTabPanel.classList.remove('front');
+        newActiveTabPanel = document.querySelector('#images');
+        newActiveTabPanel.classList.add('front');
 
-            var diva_instance = $('#diva-wrapper').data('diva');
-            diva_instance.gotoPageByName(pageName);
-            // This is to make sure the diva viewer resizes on tab change
-            // Will be a non-issue in diva 5
-            window.dispatchEvent(new Event("resize"));
-        }
+        var diva_instance = $('#diva-wrapper').data('diva');
+        diva_instance.gotoPageByName(pageName);
+        // This is to make sure the diva viewer resizes on tab change
+        // Will be a non-issue in diva 5
+        window.dispatchEvent(new Event('resize'));
     }
 
-    var folio_links = document.querySelectorAll('.folio-link');
-    for (var i=0, len=folio_links.length; i < len; i++)
-    {
-        page = folio_links[i].dataset.page;
-        folio_links[i].addEventListener("click", _onFolioClick(page) )
-    }
-
+    if (params['folio'])
+        goToPage(params['folio']);
 }
