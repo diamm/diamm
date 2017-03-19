a981e3f923fa08a247744071c359bb1391616f12
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-03-19 09:23:32 +0900

    Updated help texts

--- diamm/models/data/voice.py
+++ diamm/models/data/voice.py
@@ -5,15 +5,15 @@ class Voice(models.Model):
     class Meta:
         app_label = "diamm_data"
 
-    label = models.CharField(max_length=256, blank=True, null=True)
-    position = models.CharField(max_length=256, blank=True, null=True)
+    label = models.CharField(max_length=256, blank=True, null=True, help_text="Former Text voicepart field")
+    position = models.CharField(max_length=256, blank=True, null=True, help_text="Former Text positiononpage field")
     type = models.ForeignKey("diamm_data.VoiceType")
     item = models.ForeignKey("diamm_data.Item", related_name="voices")
     languages = models.ManyToManyField("diamm_data.Language")
     mensuration = models.ForeignKey("diamm_data.Mensuration", blank=True, null=True)
     clef = models.ForeignKey("diamm_data.Clef", blank=True, null=True)
 
-    sort_order = models.IntegerField(blank=True, null=True, help_text="Used to sort e.g., Soprano, Alto, Tenor, Bass")
+    sort_order = models.IntegerField(blank=True, null=True, help_text="Used to sort voices e.g., Soprano, Alto, Tenor, Bass")
     voice_text = models.TextField(blank=True, null=True)
     standard_text = models.ForeignKey("diamm_data.Text", blank=True, null=True)
     legacy_id = models.CharField(max_length=64, blank=True, null=True)
