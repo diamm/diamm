0510c88be44ce2819ad82c52d8c0ca978923b687
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-03-19 09:33:30 +0900

    Updated organize_images
    May not be needed...

--- diamm/management/commands/organize_images.py
+++ diamm/management/commands/organize_images.py
@@ -12,6 +12,7 @@ from diamm.models.data.source import Source
 STORE0 = "/dbdata2"
 STORE1 = "/dbdata4"
 OLDPATH = "/dbdata3/diamm/images"
+HOSTNAME = "beta.diamm.ac.uk"
 
 TEST = False
 
@@ -40,6 +41,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         signals.post_save.disconnect(index_image, sender=Image)
+        log = open('missing_images.txt', 'w')
 
         sources = Source.objects.all().select_related('archive').order_by('pk')
 
@@ -54,13 +56,12 @@ class Command(BaseCommand):
             urlpath = None
             if source.pk % 2 == 0:
                 store = STORE0
-                urlpath = "{0}{1}/store0/".format(settings.URL)
+                urlpath = "https://{0}/iiif/image/store0/".format(HOSTNAME)
             else:
                 store = STORE1
+                urlpath = "https://{0}/iiif/image/store1/".format(HOSTNAME)
 
             foldername = self._foldername(source)
-            print(foldername)
-
             pathname = os.path.join(store, foldername)
 
             # if os.path.exists(pathname):
@@ -75,15 +76,21 @@ class Command(BaseCommand):
 
             for page in source.pages.all():
                 pname = page.numeration
-                print("\t{0}".format(pname))
-                for image in page.images.filter():
+                # print("\t{0}".format(pname))
+                for image in page.images.all():
                     if not image.legacy_filename:
                         continue
 
-                    print("\t\t{0}".format(image.legacy_filename))
+                    imgurl = "{0}{1}/{2}.jp2".format(urlpath, foldername, image.legacy_filename)
+
+                    # print("\t{0}.jp2 ===> {1}".format(os.path.join(pathname, image.legacy_filename), imgurl))
 
                     if not TEST:
                         try:
                             shutil.move(os.path.join(OLDPATH, "{0}.jp2".format(image.legacy_filename)), pathname)
+                            image.location = imgurl
+                            image.save()
                         except FileNotFoundError:
+                            if source.public_images:
+                                log.write("{0}\t{1}\n".format(source.pk, image.legacy_filename))
                             pass
