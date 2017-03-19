f39c63ced5e2668bbc9d24a93e58891b8f7c6f4a
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-03-19 09:26:43 +0900

    Updated webpack config for catalogue app

--- diamm/static/apps/catalogue/webpack.config.js
+++ diamm/static/apps/catalogue/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     devtool: (process.env.NODE_ENV === "production") ? "source-map" : "eval-source-map",
     resolve: {
-        extensions: ["", ".js", ".jsx"],
+        extensions: [".js", ".jsx"],
     },
     module: {
         loaders: [
