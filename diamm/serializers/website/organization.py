3b6cd2ff1f432fa39b06f57e95217baf5297f93d
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-03-27 22:06:06 +0100

    Fixes to organization serializer

--- diamm/serializers/website/organization.py
+++ diamm/serializers/website/organization.py
@@ -4,14 +4,6 @@ from diamm.serializers.serializers import ContextSerializer, ContextDictSerializ
 from diamm.models.data.geographic_area import GeographicArea
 
 
-class OrganizationContributionSerializer(ContextSerializer):
-    contributor = serpy.StrField(
-        attr="contributor.username"
-    )
-    summary = serpy.StrField()
-    updated = serpy.StrField()
-
-
 class OrganizationLocationSerializer(ContextSerializer):
     url = serpy.MethodField()
     name = serpy.StrField(
@@ -94,7 +86,6 @@ class OrganizationDetailSerializer(ContextSerializer):
     copied_sources = serpy.MethodField()
     source_provenance = serpy.MethodField()
     location = serpy.MethodField()
-    contributors = serpy.MethodField()
 
     def get_url(self, obj):
         return reverse('organization-detail',
@@ -123,10 +114,3 @@ class OrganizationDetailSerializer(ContextSerializer):
         return OrganizationSourceProvenanceSerializer(obj.solr_provenance,
                                                       many=True,
                                                       context={"request": self.context['request']}).data
-
-    def get_contributors(self, obj):
-        if obj.contributions.count() > 0:
-            return OrganizationContributionSerializer(obj.contributions.filter(completed=True),
-                                                      context={"request": self.context['request']},
-                                                      many=True).data
-        return []
