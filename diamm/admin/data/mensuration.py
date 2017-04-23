d9d86dfb146c9cb862439cad570bf5569b520eb1
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-04-23 13:11:46 +0100

    Allow searching in admin for mensuration

--- diamm/admin/data/mensuration.py
+++ diamm/admin/data/mensuration.py
@@ -6,3 +6,4 @@ from reversion.admin import VersionAdmin
 @admin.register(Mensuration)
 class MensurationAdmin(VersionAdmin):
     list_display = ('sign', 'text')
+    search_fields = ('sign', 'text')
