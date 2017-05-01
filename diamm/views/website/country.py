8b788a4664da444e931830317dcdee067f6c1f77
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-05-01 00:04:54 -0300

    Fixes #189: United Kingdom geographical divisions
    This commit allows archives to be assigned to the devolved powers within the UK and still have them show up in the facet lists, and exist as a 'country' for the purposes of browsing cities.

--- diamm/views/website/country.py
+++ diamm/views/website/country.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework import renderers
+from django.db.models import Q
 from diamm.models.data.geographic_area import GeographicArea
 from diamm.serializers.website.country import CountryDetailSerializer
 from diamm.renderers.html_renderer import HTMLRenderer
@@ -7,6 +8,6 @@ from diamm.renderers.html_renderer import HTMLRenderer
 
 class CountryDetail(generics.RetrieveAPIView):
     template_name = "website/country/country_detail.jinja2"
-    queryset = GeographicArea.objects.filter(type=GeographicArea.COUNTRY)
+    queryset = GeographicArea.objects.filter(Q(type=GeographicArea.COUNTRY) | Q(type=GeographicArea.STATE))
     serializer_class = CountryDetailSerializer
     renderer_classes = (HTMLRenderer, renderers.JSONRenderer)
