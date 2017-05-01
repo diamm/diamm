9a9e781d1c3a382e37c9edbe63885f1ba16885e1
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-05-01 00:06:48 -0300

    Fixed: Index city variant names
    Refs #192

--- diamm/serializers/search/archive.py
+++ diamm/serializers/search/archive.py
@@ -7,12 +7,18 @@ class ArchiveSearchSerializer(ContextSerializer):
     type = serpy.MethodField()
     sources_ss = serpy.MethodField()
     city_s = serpy.StrField(attr="city.name")
+    city_variants_ss = serpy.MethodField()
     name_s = serpy.StrField(attr="name")
     country_s = serpy.StrField(attr="city.parent.name", required=False)
     siglum_s = serpy.StrField(attr="siglum")
 
     def get_type(self, obj):
         return obj.__class__.__name__.lower()
+
+    def get_city_variants_ss(self, obj):
+        if obj.city.variant_names is None:
+            return []
+        return [variant.strip() for variant in obj.city.variant_names.split(",")]
 
     def get_sources_ss(self, obj):
         return [source.display_name for source in obj.sources.all()]
