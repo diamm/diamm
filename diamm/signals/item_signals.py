9044af269877fc5f921a5ab5d452bcb8247c4351
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-06-09 18:46:42 +0200

    Fixed: Crashes when adding an item
    If there was no composition attached to an item but it was saved, the app would crash when it came time to try and index it. This commit adds a check to see if the composition is none before indexing.
    
    Fixes #208

--- diamm/signals/item_signals.py
+++ diamm/signals/item_signals.py
@@ -11,11 +11,13 @@ from diamm.serializers.search.item import ItemSearchSerializer
 def index_item(sender, instance, created, **kwargs):
     solr_index(ItemSearchSerializer, instance)
     solr_index(SourceSearchSerializer, instance.source)
-    solr_index(CompositionSearchSerializer, instance.composition)
+    if instance.composition:
+        solr_index(CompositionSearchSerializer, instance.composition)
 
 
 @receiver(post_delete, sender=Item)
 def delete_item(sender, instance, **kwargs):
     solr_delete(instance)
     solr_index(SourceSearchSerializer, instance.source)
-    solr_index(CompositionSearchSerializer, instance.composition)
+    if instance.composition:
+        solr_index(CompositionSearchSerializer, instance.composition)
