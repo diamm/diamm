e081c2bfd655592ca4539b993eea88269f833f83
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-07-16 13:40:26 +0100

    Docs: Help text for item composers

--- diamm/models/data/item_composer.py
+++ diamm/models/data/item_composer.py
@@ -5,9 +5,14 @@ class ItemComposer(models.Model):
     class Meta:
         app_label = "diamm_data"
 
+    COMPOSER_HELP_TEXT = """DO NOT attach a duplicate composer here if this item is attached to a composition.
+                            This field is ONLY to be used to record that a MSS features works by a particular composer,
+                            and should not be used otherwise."""
+
     item = models.ForeignKey("diamm_data.Item",
                              related_name="unattributed_composers")
     composer = models.ForeignKey("diamm_data.Person",
-                                 related_name="unattributed_works")
+                                 related_name="unattributed_works",
+                                 help_text=COMPOSER_HELP_TEXT)
     uncertain = models.BooleanField(default=False)
     note = models.TextField(blank=True, null=True)
