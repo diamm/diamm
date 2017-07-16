4f3ddd7fc9e5889d2ecb3b2047202dfe9a9cf9e5
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-07-16 15:23:32 +0100

    Fixed: Remove legacy models
    No longer needed

--- diamm/models/__init__.py
+++ diamm/models/__init__.py
@@ -9,40 +9,40 @@ from diamm.models.site.dissertation_page import DissertationPage
 from diamm.models.site.home_page import HomePage
 
 
-from diamm.models.migrate.legacy_source import LegacySource
-from diamm.models.migrate.legacy_archive import LegacyArchive
-from diamm.models.migrate.legacy_country import LegacyCountry
-from diamm.models.migrate.legacy_city import LegacyCity
-from diamm.models.migrate.legacy_composer import LegacyComposer
-from diamm.models.migrate.legacy_copyist import LegacyCopyist
-from diamm.models.migrate.legacy_language import LegacyLanguage
-from diamm.models.migrate.legacy_person import LegacyPerson
-from diamm.models.migrate.legacy_source_copyist import LegacySourceCopyist
-from diamm.models.migrate.legacy_bibliography import LegacyBibliography
-from diamm.models.migrate.legacy_author import LegacyAuthor
-from diamm.models.migrate.legacy_author_bibliography import LegacyAuthorBibliography
-from diamm.models.migrate.legacy_bibliography_source import LegacyBibliographySource
-from diamm.models.migrate.legacy_relationship_type import LegacyRelationshipType
-from diamm.models.migrate.legacy_source_person import LegacySourcePerson
-from diamm.models.migrate.legacy_source_notation import LegacySourceNotation
-from diamm.models.migrate.legacy_composition import LegacyComposition
-from diamm.models.migrate.legacy_composition_composer import LegacyCompositionComposer
-from diamm.models.migrate.legacy_item import LegacyItem
-from diamm.models.migrate.legacy_image import LegacyImage
-from diamm.models.migrate.legacy_secondary_image import LegacySecondaryImage
-from diamm.models.migrate.legacy_item_image import LegacyItemImage
-from diamm.models.migrate.legacy_provenance import LegacyProvenance
-from diamm.models.migrate.legacy_set import LegacySet
-from diamm.models.migrate.legacy_source_set import LegacySourceSet
-from diamm.models.migrate.legacy_clef import LegacyClef
-from diamm.models.migrate.legacy_text import LegacyText
-from diamm.models.migrate.legacy_text_language import LegacyTextLanguage
-from diamm.models.migrate.legacy_notation import LegacyNotation
-from diamm.models.migrate.legacy_bibliography_item import LegacyBibliographyItem
-from diamm.models.migrate.legacy_cycle_type import LegacyCycleType
-from diamm.models.migrate.legacy_composition_cycle import LegacyCompositionCycle
-from diamm.models.migrate.legacy_composition_cycle_composition import LegacyCompositionCycleComposition
-from diamm.models.migrate.legacy_note import LegacyNote
+# from diamm.models.migrate.legacy_source import LegacySource
+# from diamm.models.migrate.legacy_archive import LegacyArchive
+# from diamm.models.migrate.legacy_country import LegacyCountry
+# from diamm.models.migrate.legacy_city import LegacyCity
+# from diamm.models.migrate.legacy_composer import LegacyComposer
+# from diamm.models.migrate.legacy_copyist import LegacyCopyist
+# from diamm.models.migrate.legacy_language import LegacyLanguage
+# from diamm.models.migrate.legacy_person import LegacyPerson
+# from diamm.models.migrate.legacy_source_copyist import LegacySourceCopyist
+# from diamm.models.migrate.legacy_bibliography import LegacyBibliography
+# from diamm.models.migrate.legacy_author import LegacyAuthor
+# from diamm.models.migrate.legacy_author_bibliography import LegacyAuthorBibliography
+# from diamm.models.migrate.legacy_bibliography_source import LegacyBibliographySource
+# from diamm.models.migrate.legacy_relationship_type import LegacyRelationshipType
+# from diamm.models.migrate.legacy_source_person import LegacySourcePerson
+# from diamm.models.migrate.legacy_source_notation import LegacySourceNotation
+# from diamm.models.migrate.legacy_composition import LegacyComposition
+# from diamm.models.migrate.legacy_composition_composer import LegacyCompositionComposer
+# from diamm.models.migrate.legacy_item import LegacyItem
+# from diamm.models.migrate.legacy_image import LegacyImage
+# from diamm.models.migrate.legacy_secondary_image import LegacySecondaryImage
+# from diamm.models.migrate.legacy_item_image import LegacyItemImage
+# from diamm.models.migrate.legacy_provenance import LegacyProvenance
+# from diamm.models.migrate.legacy_set import LegacySet
+# from diamm.models.migrate.legacy_source_set import LegacySourceSet
+# from diamm.models.migrate.legacy_clef import LegacyClef
+# from diamm.models.migrate.legacy_text import LegacyText
+# from diamm.models.migrate.legacy_text_language import LegacyTextLanguage
+# from diamm.models.migrate.legacy_notation import LegacyNotation
+# from diamm.models.migrate.legacy_bibliography_item import LegacyBibliographyItem
+# from diamm.models.migrate.legacy_cycle_type import LegacyCycleType
+# from diamm.models.migrate.legacy_composition_cycle import LegacyCompositionCycle
+# from diamm.models.migrate.legacy_composition_cycle_composition import LegacyCompositionCycleComposition
+# from diamm.models.migrate.legacy_note import LegacyNote
 
 
 from diamm.models.data.legacy_id import LegacyId  # Yes, this belongs here. Used to track old PKs
@@ -73,7 +73,7 @@ from diamm.models.data.item_note import ItemNote
 from diamm.models.data.item_composer import ItemComposer
 from diamm.models.data.page import Page
 from diamm.models.data.page_note import PageNote
-from diamm.models.data.page_condition import PageCondition
+# from diamm.models.data.page_condition import PageCondition
 from diamm.models.data.image import Image
 from diamm.models.data.image_note import ImageNote
 from diamm.models.data.image_type import ImageType
