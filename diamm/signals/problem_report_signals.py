8165cda0ca48b02ef868ae4cc2bc06de6c5c1399
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-07-26 21:25:41 +0100

    New: E-mail sent when a problem report has been accepted
    Fixes #283

--- diamm/signals/problem_report_signals.py
+++ diamm/signals/problem_report_signals.py
@@ -5,6 +5,8 @@ from diamm.models.site.problem_report import ProblemReport
 from diamm.models.diamm_user import CustomUserModel
 from django.db.models.signals import post_save
 
+FRIENDLY_FROM = "Digital Image Archive of Medieval Music <{0}>".format(settings.DEFAULT_FROM_EMAIL)
+
 
 @receiver(post_save, sender=ProblemReport)
 def send_thank_you_email(sender, instance, created, **kwargs):
@@ -26,12 +28,14 @@ def send_thank_you_email(sender, instance, created, **kwargs):
     send_mail(
         "Thank you for your contribution",
         email_message,
-        settings.DEFAULT_FROM_EMAIL,
+        FRIENDLY_FROM,
         [email_address],
         fail_silently=False
     )
 
 
+# When an issue has been reported, send an e-mail to all people listed as an
+# "Editor".
 @receiver(post_save, sender=ProblemReport)
 def send_admin_notification_email(sender, instance, created, **kwargs):
     if not created:
@@ -57,7 +61,44 @@ def send_admin_notification_email(sender, instance, created, **kwargs):
     send_mail(
         "A new correction report is available",
         email_message,
-        settings.DEFAULT_FROM_EMAIL,
+        FRIENDLY_FROM,
         recipients,
+        fail_silently=False
+    )
+
+
+# When an issue has been resolved, send an e-mail to the issue reporter letting them know
+#  that it has been fixed.
+@receiver(post_save, sender=ProblemReport)
+def send_issue_resolved_message(sender, instance, created, **kwargs):
+    if not instance.accepted:
+        return None
+
+    reported_entity = instance.record
+    reporter = instance.contributor
+    summary = instance.summary
+    original_report = instance.note
+    email_address = instance.contributor.get_username()
+
+    name = reporter.full_name
+    record = reported_entity.display_name
+
+    if settings.DEBUG:
+        recipient = [settings.ADMIN_EMAIL]
+    else:
+        recipient = [email_address]
+
+    email_message = settings.MAIL["ISSUE_RESOLVED"].format(
+        name=name,
+        record=record,
+        summary=summary,
+        original_report=original_report
+    )
+
+    send_mail(
+        "Thank you for your contribution",
+        email_message,
+        FRIENDLY_FROM,
+        recipient,
         fail_silently=False
     )
