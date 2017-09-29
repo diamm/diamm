343c87d46e956902364b88abd6af20c7efaf69c2
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-09-29 18:09:47 +0100

    Fixed: Standardize uncertain field names

--- diamm/models/data/person_role.py
+++ diamm/models/data/person_role.py
@@ -10,9 +10,9 @@ class PersonRole(models.Model):
     role = models.ForeignKey("diamm_data.Role",
                              related_name="people")
     earliest_year = models.IntegerField(blank=True, null=True)
-    earliest_year_uncertain = models.BooleanField(default=False)
+    earliest_year_approximate = models.BooleanField(default=False)
     latest_year = models.IntegerField(blank=True, null=True)
-    latest_year_uncertain = models.BooleanField(default=False)
+    latest_year_approximate = models.BooleanField(default=False)
     note = models.TextField(blank=True, null=True)
 
     def full_role(self):
