e9fa5c43353476c78af850fbc9e6627253f842fe
Andrew Hankinson <andrew.hankinson@gmail.com>
2024-06-22 16:10:57 +0200

    Fixed: Crash when adding users
    Fixes #589

--- diamm/admin/diamm_user.py
+++ diamm/admin/diamm_user.py
@@ -109,7 +109,7 @@ class UserAdmin(BaseUserAdmin):
         (None, {
             'classes': ('wide',),
             'fields': ('email', 'password1', 'password2')
-        })
+        }),
     )
 
     search_fields = ('email', 'affiliation', 'last_name', 'first_name')
