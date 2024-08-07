f49a46c6be9a033306758ed2898a7cd6e0d822e6
Andrew Hankinson <andrew.hankinson@gmail.com>
2024-08-07 08:04:44 +0200

    Fixed: Instance pk was none
    This fixes an issue where the instance pk was set to None when trying to delete an item record in Solr.

--- diamm/signals/item_signals.py
+++ diamm/signals/item_signals.py
@@ -1,6 +1,6 @@
 import threading
 
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_delete
 from django.dispatch import receiver
 
 from diamm.helpers.solr_helpers import solr_delete, solr_index, SolrConnection
@@ -28,18 +28,18 @@ def __composer_inventory_index(item):
             SolrConnection.delete(id=doc['id'])
 
     res = [list(o) for o in source.inventory.values_list(*FIELDS_TO_INDEX)]
-    data = ComposerInventorySearchSerializer(res, many=True).data
+    data: dict = ComposerInventorySearchSerializer(res, many=True).data
     SolrConnection.add(data)
     SolrConnection.commit()
 
 
-def __composer_inventory_delete(item):
+def __composer_inventory_delete(item, item_pk):
     source = item.source
     # conn = pysolr.Solr(settings.SOLR['SERVER'])
 
     fq = ["type:composerinventory",
           f"source_i:{source.pk}",
-          f"item_i:{item.pk}"]
+          f"item_i:{item_pk}"]
     records = SolrConnection.search("*:*", fq=fq, fl="id")
     if records.docs:
         for doc in records.docs:
@@ -61,9 +61,10 @@ def index_item(sender, instance, created, **kwargs):
     # __composer_inventory_index(instance)
 
 
-@receiver(post_delete, sender=Item)
+@receiver(pre_delete, sender=Item)
 def delete_item(sender, instance, **kwargs):
-    t = threading.Thread(target=_do_item_delete, args=(instance,))
+    print("pre-delete", instance.pk)
+    t = threading.Thread(target=_do_item_delete, args=(instance, instance.pk))
     t.daemon = True
     t.start()
     # solr_delete(instance)
@@ -91,11 +92,12 @@ def _do_item_index(instance):
     __composer_inventory_index(instance)
 
 
-def _do_item_delete(instance):
+def _do_item_delete(instance, instance_pk):
     print("Threaded item delete")
     solr_delete(instance)
+    __composer_inventory_delete(instance, instance_pk)
+
     solr_index(SourceSearchSerializer, instance.source)
     if instance.composition:
         solr_index(CompositionSearchSerializer, instance.composition)
 
-    __composer_inventory_delete(instance)
