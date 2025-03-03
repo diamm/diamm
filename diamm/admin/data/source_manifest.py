40a7c62d506bac9a13dd1371e357ba7d7c6b7482
Andrew Hankinson <andrew.hankinson@gmail.com>
2025-03-03 21:12:50 +0100

    Delete source manifest

--- diamm/admin/data/source_manifest.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from diamm.models.data.source_manifest import SourceManifest
-
-
-@admin.register(SourceManifest)
-class SourceManifestAdmin(admin.ModelAdmin):
-    pass
