aa7ebc527c638b1bff3df670b21d99a48c1e8d5e
Andrew Hankinson <andrew.hankinson@gmail.com>
2025-11-12 09:42:29 +0100

    Add count for number of sources

--- diamm/admin/site/project_sources.py
+++ diamm/admin/site/project_sources.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count
 
 from diamm.models import ProjectSources
 
@@ -7,3 +8,12 @@ from diamm.models import ProjectSources
 class ProjectSourcesAdmin(admin.ModelAdmin):
     filter_horizontal = ["sources"]
     prepopulated_fields = {"slug": ["project"]}
+    list_display = ("project", "source_count")
+
+    @admin.display(description="Number of sources")
+    def source_count(self, obj) -> str:
+        return f"{obj.source_count}"
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.annotate(source_count=Count("sources"))
