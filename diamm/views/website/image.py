1e4353ec3724fb33576a5607bfcb37245684c6c9
Andrew Hankinson <andrew.hankinson@gmail.com>
2026-07-14 08:57:24 +0200

    Fix ID value

--- diamm/views/website/image.py
+++ diamm/views/website/image.py
@@ -259,7 +259,7 @@ def _fetch_iip_info_json(
     request: HttpRequest, pk: int, location: str
 ) -> dict[str, Any] | None:
     del pk
-    info_url = f"{settings.DIAMM_IMAGE_SERVER}{location}/info.json"
+    info_url = f"{settings.DIAMM_IMAGE_SERVER}{location}"
     headers = {
         "referer": f"https://{settings.HOSTNAME}",
         "X-IIIF-ID": request.build_absolute_uri(request.path),
